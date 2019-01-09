874024373b93e33f399e38e4a274bb831681ad1b
Furl0w <rainbowphoque@gmail.com>
2019-01-09 11:02:54 +0100

    adding mux router, correcting encoding in json of every valid return

--- app/main.go
+++ app/main.go
@@ -3,11 +3,27 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"serverMongoDB/db"
+	"strings"
+	"unicode"
+
+	"github.com/gorilla/mux"
+	"github.com/mongodb/mongo-go-driver/mongo"
 )
+
+var client *mongo.Client
+
+type creationRequest struct {
+	Name string `json:"name,omitempty"`
+}
+
+type successInsert struct {
+	ID string `json:"id,omitempty"`
+}
 
 func main() {
 	var dbPort, hostName, port string
@@ -21,74 +37,113 @@ func main() {
 		hostName = "localhost"
 	}
 
-	client, err := db.InitDB(hostName, dbPort)
+	c, err := db.InitDB(hostName, dbPort)
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
+	client = c
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Initiating connection with DB\n")
-		err := db.PingDBClient(client)
-		if err != nil {
-			fmt.Fprintf(w, "%s\n", err.Error())
-			fmt.Fprintf(w, "Failed to connect to DB")
-		} else {
-			fmt.Fprintf(w, "Succeeded to connect to DB")
-		}
+	r := mux.NewRouter()
+	r.HandleFunc("/", pingDB).Methods("GET")
+	r.HandleFunc("/users", getUsers).Methods("GET")
+	r.HandleFunc("/user/{id}", getUser).Methods("GET")
+	r.HandleFunc("/user", createUser).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":"+port, r))
+}
+
+func pingDB(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintf(w, "Initiating connection with DB\n")
+	err := db.PingDBClient(client)
+	if err != nil {
+		w.WriteHeader(500)
+		log.Print(err.Error())
+		fmt.Fprintf(w, "Failed to connect to DB")
 		return
-	})
+	}
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Succeeded to connect to DB")
+	return
+}
 
-	http.HandleFunc("/retrieve", func(w http.ResponseWriter, r *http.Request) {
-		results, err := db.RetrieveUserByID(client, "5c34835d30195d52cb03b7c2")
-		if err != nil {
-			w.WriteHeader(500)
-			fmt.Fprintf(w, "error when finding stuff\n")
-			fmt.Fprintf(w, "%s\n", err.Error())
-			return
-		}
-		jsonString, err := json.Marshal(results)
-		if err != nil {
-			w.WriteHeader(500)
-			fmt.Fprintf(w, "error when marshalling json\n")
-			fmt.Fprintf(w, "%s\n", err.Error())
-			return
-		}
-		w.Header().Add("Content-Type", "application/json")
-		fmt.Fprintf(w, "%s", jsonString)
+func getUsers(w http.ResponseWriter, r *http.Request) {
+
+	results, err := db.RetrieveUsers(client)
+	if err != nil {
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "error when finding stuff\n")
+		fmt.Fprintf(w, "%s\n", err.Error())
 		return
-	})
-
-	http.HandleFunc("/retrieves", func(w http.ResponseWriter, r *http.Request) {
-		results, err := db.RetrieveUsers(client)
-		if err != nil {
-			w.WriteHeader(500)
-			fmt.Fprintf(w, "error when finding stuff\n")
-			fmt.Fprintf(w, "%s\n", err.Error())
-			return
-		}
-		jsonString, err := json.Marshal(results)
-		if err != nil {
-			w.WriteHeader(500)
-			fmt.Fprintf(w, "error when marshalling json\n")
-			fmt.Fprintf(w, "%s\n", err.Error())
-			return
-		}
-		w.Header().Add("Content-Type", "application/json")
-		fmt.Fprintf(w, "%s", jsonString)
+	}
+	jsonString, err := json.Marshal(results)
+	if err != nil {
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "error when marshalling json\n")
+		log.Print(err.Error())
 		return
-	})
+	}
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "%s", jsonString)
+	return
+}
 
-	http.HandleFunc("/insert", func(w http.ResponseWriter, r *http.Request) {
-		id, err := db.InsertUser(client, "Yo yo")
-		if err != nil {
-			fmt.Fprintf(w, "error when finding stuff\n")
-			fmt.Fprintf(w, "%s\n", err.Error())
-			return
-		}
-		fmt.Fprint(w, id)
+func getUser(w http.ResponseWriter, r *http.Request) {
+
+	params := mux.Vars(r)
+	results, err := db.RetrieveUserByID(client, params["id"])
+	if err != nil {
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "error when finding stuff\n")
+		log.Print(err.Error())
 		return
-	})
+	}
+	jsonString, err := json.Marshal(results)
+	if err != nil {
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "error when marshalling json\n")
+		log.Print(err.Error())
+		return
+	}
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "%s", jsonString)
+	return
+}
 
-	http.ListenAndServe(":"+port, nil)
+func createUser(w http.ResponseWriter, r *http.Request) {
+
+	var userRequest creationRequest
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "error when receiving stuff\n")
+		log.Print(err.Error())
+		return
+	}
+	err = json.Unmarshal(body, &userRequest)
+	if err != nil {
+		w.WriteHeader(403)
+		fmt.Fprintf(w, "error when unmarshalling json\n")
+		log.Print(err.Error())
+		return
+	}
+	id, err := db.InsertUser(client, userRequest.Name)
+	if err != nil {
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "error when inserting stuff\n")
+		log.Print(err.Error())
+		return
+	}
+	w.Header().Set("Content-type", "application/json;charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	id = strings.TrimFunc(id, func(r rune) bool {
+		return r == '"' || unicode.IsSpace(r)
+	})
+	res := successInsert{ID: id}
+	json, err := json.Marshal(res)
+	fmt.Fprint(w, string(json))
+	return
 }
